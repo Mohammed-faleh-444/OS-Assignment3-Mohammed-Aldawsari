d52deaa2081c6eb602006f5f9ad80c2c60e1b93e
Mohammed-faleh-444 <444050971@std.psau.edu.sa>
2026-05-02 18:48:00 +0300

    task 3: add semaphore to control concurrent CPU access

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -116,8 +116,8 @@ class Process implements Runnable {
     public void run() {
         // TODO #3: Acquire CPU semaphore before executing
         // This ensures only allowed number of processes run simultaneously
-        
         try {
+            SharedResources.cpuSemaphore.acquire();
             if (startTime == -1) {
                 startTime = System.currentTimeMillis();
             }
@@ -176,9 +176,10 @@ class Process implements Runnable {
             }
             System.out.println();
             
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
-            // TODO #4: Release CPU semaphore here
-            // Always release in finally block to prevent deadlocks!
+            SharedResources.cpuSemaphore.release();
         }
     }
     
